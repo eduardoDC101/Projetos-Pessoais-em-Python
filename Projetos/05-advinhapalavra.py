1fab8b5fae1e584659e324dbe1638fae418dd616
Eduardo Dourado <166930671+eduardoDC101@users.noreply.github.com>
2024-07-02 12:34:02 -0300

    Add files via upload

---
+++ Projetos/05-advinhapalavra.py
@@ -0,0 +1,57 @@
+import os 
+os.system("cls")
+
+print("="*16," ( Jogo - Advinhe a Palavra ) ","="*16,end="\n\n")
+print('Digite\n"stop" para sair do jogo ou\n"cls" para limpar a tela\n')
+os.system("PAUSE")
+
+os.system("CLS")
+PALAVRA_SECRETA = "Security"
+tamanho_da_palavra = len(PALAVRA_SECRETA)
+contador =0
+letras_acertadas = ""
+
+letra = ""
+
+while letra != "stop":
+    letra = input("Digite uma letra: ")
+    try:
+        var_teste = int(letra)
+        if(type(var_teste) == int):
+            print("Você digitou um número não uma letra! ")
+            continue
+    except:
+        if(letra.lower() =="cls"):
+            os.system("CLS")
+
+        elif(letra.lower() == "stop"):
+            break
+
+        elif(len(letra)>1):
+            print("Digite apenas uma letra")
+            continue 
+        
+        elif(len(letra)==0):
+            print("Você não digitou uma letra! ")
+            continue
+        
+        contador +=1 
+
+        if(letra in PALAVRA_SECRETA):
+            letras_acertadas += letra
+        
+        palavra_formada = ""
+        for letra_secreta in PALAVRA_SECRETA:
+            if letra_secreta in letras_acertadas:
+                palavra_formada +=letra_secreta
+            else:
+                palavra_formada+="*"
+        
+        print("Palavra Secreta: ",palavra_formada)
+
+        if(palavra_formada == PALAVRA_SECRETA):
+            print("VOCÊ GANHOU!!!")
+            print("A palavra era: ", PALAVRA_SECRETA)
+            print("Tentativas: ",contador)
+            contador =0
+            letras_acertadas = ""
